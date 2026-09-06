216f4f21a49748cef87ec94c47ef7ce4d80d40ba
calvincao <calvincao@qifalab.com>
2026-09-06 22:30:55 +0800

    fix: avoid duplicate React development mounts

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -15,7 +14,6 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <AuthProvider>
@@ -24,7 +22,6 @@ root.render(
         </BrowserRouter>
       </AuthProvider>
     </ThemeProvider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
